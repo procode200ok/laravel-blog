1322560acc6d63da25291d9782a68340528ddaf7
procode.200ok <procode.200ok@gmail.com>
2023-08-02 18:22:36 +0530

    update exception

--- app/Exceptions/ApiExceptions.php
+++ app/Exceptions/ApiExceptions.php
@@ -11,34 +11,34 @@ use Illuminate\Validation\ValidationException;
 class ApiExceptions extends Exception
 {
     // HTTP status code for the exception
-    protected $statusCode;
+   protected $statusCode;
 
-    // Custom message for the exception
-    protected $message;
+   // Custom message for the exception
+   protected $message;
 
-    public function __construct($data = [], $rules = [], $messages = [], $customAttributes = [], $statusCode = null, \Throwable $previous = null)
-    {
-        $this->message = 'Internal Server Error.';
-        $this->statusCode = $statusCode ?? Response::HTTP_INTERNAL_SERVER_ERROR;
+   public function __construct($data = [], $rules = [], $messages = [], $customAttributes = [], $statusCode = null, \Throwable $previous = null)
+   {
+      $this->message = 'Internal Server Error.';
+      $this->statusCode = $statusCode ?? Response::HTTP_INTERNAL_SERVER_ERROR;
 
-        parent::__construct($this->message, $this->statusCode, $previous);
+      parent::__construct($this->message, $this->statusCode, $previous);
 
-        $this->validateOrFail($data, $rules, $messages, $customAttributes);
-    }
+      $this->validateOrFail($data, $rules, $messages, $customAttributes);
+   }
 
-    public function render()
-    {
-        return response()->json(['error' => $this->message], $this->statusCode);
-    }
+   public function render()
+   {
+      return response()->json(['error' => $this->message], $this->statusCode);
+   }
 
-    // Method to handle validation and throw ValidationException inside CustomException
-    public function validateOrFail($data, $rules, $messages = [], $customAttributes = [])
-    {
-        $validator = Validator::make($data, $rules, $messages, $customAttributes);
+   // Method to handle validation and throw ValidationException inside CustomException
+   public function validateOrFail($data, $rules, $messages = [], $customAttributes = [])
+   {
+      $validator = Validator::make($data, $rules, $messages, $customAttributes);
 
-        if ($validator->fails()) {
-            throw new ValidationException($validator);
-        }
-    }
+      if ($validator->fails()) {
+         throw new ValidationException($validator);
+      }
+   }
 
 }
